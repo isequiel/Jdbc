e2891921d5f8b74a3d5860a15dfe744d9a259372
isequiel_borges <isequiel_borges@10.1.48.164>
2013-08-02 17:11:57 -0300

    Crie a classe MarcaDAOJDBC

---
+++ CrudJDBC/src/br/com/modelo/persistencia/MarcaDAO.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.modelo.persistencia;
+
+import br.com.modelo.negocio.Marca;
+import java.util.List;
+
+/**
+ *
+ * @author isequiel_borges
+ */
+public interface MarcaDAO {
+    
+    void inserir(Marca m);
+    
+    void atualizar(Marca m);
+    
+    void excluir(int id);
+    
+    List<Marca> getMarcas();
+    
+    List<Marca> getMarcasByNome(String nome);
+    
+    Marca getMarcasById(int id);
+    
+    
+    
+}
