8588987ffef337e898b9b6a82bdff88c562430e9
isequiel_borges <isequiel_borges@10.1.48.164>
2013-08-02 17:12:42 -0300

    Crie a classe MarcaController

---
+++ CrudJDBC/src/br/com/modelo/controller/MarcaController.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.modelo.controller;
+
+import br.com.modelo.negocio.Marca;
+import br.com.modelo.persistencia.MarcaDAO;
+import br.com.modelo.persistencia.MarcaDAOJDBC;
+import java.util.List;
+
+/**
+ *
+ * @author isequiel_borges
+ */
+public class MarcaController {
+    /*
+     * 
+     */
+    public void inserir (Marca m){
+        MarcaDAO md = new MarcaDAOJDBC();
+        md.atualizar(m);
+    }
+    /*
+     * 
+     */
+    public void excluir(int id){
+        MarcaDAO md = new MarcaDAOJDBC();
+        md.excluir(id);
+    }
+    /*
+     * 
+     */
+    public List<Marca> getMarca(){
+      MarcaDAO md = new MarcaDAOJDBC(); 
+      return md.getMarcas();   
+    } 
+    /*
+     * 
+     */
+    public List<Marca> getMarcaByNome(String nome){
+      MarcaDAO md = new MarcaDAOJDBC(); 
+      return md.getMarcasByNome(nome);   
+    } 
+    /*
+     * 
+     */
+    public Marca getMarcaById(int id){
+      MarcaDAO md = new MarcaDAOJDBC(); 
+      return md.getMarcasById(id);   
+    } 
+}
+
