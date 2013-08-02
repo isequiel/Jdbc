e0fc6cf3a03481c3da5eea14516dc8ed3c60775e
isequiel_borges <isequiel_borges@10.1.48.164>
2013-08-02 17:12:17 -0300

    Crie a classe FabricaConexao

---
+++ CrudJDBC/src/br/com/modelo/persistencia/FabricaConexao.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.modelo.persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author isequiel_borges
+ */
+public class FabricaConexao {
+    
+    public static Connection getConnection() throws SQLException{
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            return DriverManager.getConnection("jdbc:mysql://3306/revenda", "root", "");
+        }catch (ClassNotFoundException e){
+            throw  new SQLException(e.getMessage());
+        }
+    }
+    
+}
