f8692fa0c1c83e413afd5723d337c94579cba621
isequiel_borges <isequiel_borges@10.1.48.164>
2013-08-08 17:19:16 -0300

    Crie a classe MarcaControllera

--- CrudJDBC/src/br/com/view/AtualizarMarca.java
+++ CrudJDBC/src/br/com/view/AtualizarMarca.java
@@ -4,17 +4,30 @@
  */
 package br.com.view;
 
+import br.com.modelo.controller.MarcaController;
+import br.com.modelo.negocio.Marca;
+
 /**
  *
  * @author isequiel_borges
  */
 public class AtualizarMarca extends javax.swing.JFrame {
-
+     MarcaTableModel modelo;
+     int linhaSelecionada;
+     
     /**
      * Creates new form AtualizarMarca
      */
-    public AtualizarMarca() {
+    public AtualizarMarca(int linhaSelecionada, int idMarca, MarcaTableModel modelo) {
+        this.linhaSelecionada = linhaSelecionada;
+        this.modelo = modelo;
         initComponents();
+        
+        Marca m = new Marca();
+        MarcaController mc = new MarcaController();
+        m = mc.getMarcaById(idMarca);
+        txId.setText((Integer.toString(m.getId())));
+        txMarca.setText(m.getNome());
     }
 
     /**
@@ -29,20 +42,21 @@ public class AtualizarMarca extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
+        txId = new javax.swing.JTextField();
+        txMarca = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
         setMinimumSize(new java.awt.Dimension(500, 350));
         getContentPane().setLayout(null);
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 3, 14)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Wide Latin", 3, 24)); // NOI18N
         jLabel1.setText("Atualizar Marca");
         getContentPane().add(jLabel1);
-        jLabel1.setBounds(160, 30, 150, 17);
+        jLabel1.setBounds(80, 19, 390, 40);
 
         jLabel2.setText("Id:");
         getContentPane().add(jLabel2);
@@ -51,65 +65,54 @@ public class AtualizarMarca extends javax.swing.JFrame {
         jLabel3.setText("Nome:");
         getContentPane().add(jLabel3);
         jLabel3.setBounds(20, 140, 40, 14);
-        getContentPane().add(jTextField1);
-        jTextField1.setBounds(60, 90, 230, 30);
-        getContentPane().add(jTextField2);
-        jTextField2.setBounds(60, 130, 230, 30);
+        getContentPane().add(txId);
+        txId.setBounds(60, 90, 290, 30);
+        getContentPane().add(txMarca);
+        txMarca.setBounds(60, 130, 290, 30);
 
         jButton1.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jButton1.setText("Limpar");
         getContentPane().add(jButton1);
-        jButton1.setBounds(270, 250, 73, 23);
+        jButton1.setBounds(263, 250, 80, 30);
 
         jButton2.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jButton2.setText("Salvar");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton2);
-        jButton2.setBounds(350, 250, 63, 23);
+        jButton2.setBounds(350, 250, 90, 30);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        getContentPane().add(jPanel1);
+        jPanel1.setBounds(0, 0, 510, 320);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AtualizarMarca.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AtualizarMarca.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AtualizarMarca.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AtualizarMarca.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+       Marca m = new Marca();
+       m.setId((Integer.parseInt(txId.getText())));
+       m.setNome(txMarca.getText());
+       
+       MarcaController mc = new MarcaController();
+       mc.atualizar(m);
+       
+       modelo.updateMarca(linhaSelecionada, m);
+       dispose();
+    }//GEN-LAST:event_jButton2ActionPerformed
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new AtualizarMarca().setVisible(true);
-            }
-        });
-    }
+  
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField txId;
+    private javax.swing.JTextField txMarca;
     // End of variables declaration//GEN-END:variables
 }
